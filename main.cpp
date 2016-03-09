44ecdd12becdd6c8c4963420abeed8dd2af0895b
Najib Arbaoui <arbaouinajib@gmail.com>
2016-03-09 18:43:03 -0500

    add vorace center

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <random>
 #include <ctime>
+#include <list>
 #include "pair_tid.h"
 #include "solution.h"
 
@@ -47,7 +48,7 @@ bool isAPair(Tid tid_1, Tid tid_2, vector<PairTid> data) {
     return false;
 }
 
-bool Vorace(vector<PairTid> &data, Solution &solution) {
+void Vorace(vector<PairTid> &data, Solution &solution) {
 
     for (auto item : data) {
         if (!solution.isExist(item.getEntry().first))
@@ -57,6 +58,41 @@ bool Vorace(vector<PairTid> &data, Solution &solution) {
     }
     solution.setPosition();
 }
+
+void VoraceCenter(vector<PairTid> &data, Solution &solution) {
+    bool toggle = false;
+    list<Tid> temp;
+    for (auto item : data) {
+        if (!toggle) {
+            if (!solution.isExist(item.getEntry().first)) {
+                solution.tids.push_back(item.getEntry().first);
+                temp.push_back(item.getEntry().first);
+            }
+            if (!solution.isExist(item.getEntry().second)) {
+                solution.tids.push_back(item.getEntry().second);
+                temp.push_back(item.getEntry().second);
+            }
+            toggle = !toggle;
+        } else {
+            if (!solution.isExist(item.getEntry().first)) {
+                solution.tids.push_back(item.getEntry().first);
+                temp.push_front(item.getEntry().first);
+            }
+            if (!solution.isExist(item.getEntry().second)) {
+                solution.tids.push_back(item.getEntry().second);
+                temp.push_front(item.getEntry().second);
+            }
+            toggle = !toggle;
+
+        }
+    }
+    for(int i=0;i<solution.tids.size();++i){
+        solution.tids[i] = temp.front();
+        temp.pop_front();
+    }
+    solution.setPosition();
+}
+
 
 int getDistanceMax(Solution solution, vector<PairTid> data, int &i_1, int &i_2) {
     //   Afficher les distances
@@ -104,7 +140,7 @@ void swapTid(Solution solution, int &i_1, int &i_2) {
 void WriteSolution(Solution solution, string file_name, vector<PairTid> data) {
     int i_1, i_2;
     std::ofstream myfile;
-    std::string file_path_out = "result/" + file_name + "_solution.txt";
+    std::string file_path_out = "result/vorace_center/" + file_name + "_solution.txt";
     myfile.open(file_path_out, std::fstream::app);
     myfile << "Distance max : " << getDistanceMax(solution, data, i_1, i_2) << std::endl;
     for (auto item : solution.tids) {
@@ -123,9 +159,10 @@ int main() {
     int i_1 = 0, i_2 = 0;
     vector<PairTid> data;
     Solution solution;
-    std::string file_in = "trace_1000_sec_cpu0";
-    ReadFile("data/"+file_in + ".txt", data);
-    Vorace(data, solution);
+    std::string file_in = "trace_1000_sec_cpu1";
+    ReadFile("data/" + file_in + ".txt", data);
+//    Vorace(data, solution);
+    VoraceCenter(data, solution);
 
     Solution solution_temp = solution;
     distance_max = getDistanceMax(solution, data, i_1, i_2);
@@ -137,27 +174,27 @@ int main() {
     double duration;
     start = std::clock();
 
-    while (duration < 1.0) {
-        swapTid(solution_temp, i_1, i_2);
-        if (distance_max > getDistanceMax(solution_temp, data, i_1, i_2)) {
-            solution = solution_temp;
-            distance_max = getDistanceMax(solution, data, i_1, i_2);
-            std::cout << "__________________________ " << std::endl;
-            std::cout << "Distance max : " << getDistanceMax(solution, data, i_1, i_2) << std::endl;
-        }
-        duration = (std::clock() - start) / (double) CLOCKS_PER_SEC;
-
-    }
+//    while (duration < 1.0) {
+//        swapTid(solution_temp, i_1, i_2);
+//        if (distance_max > getDistanceMax(solution_temp, data, i_1, i_2)) {
+//            solution = solution_temp;
+//            distance_max = getDistanceMax(solution, data, i_1, i_2);
+//            std::cout << "__________________________ " << std::endl;
+//            std::cout << "Distance max : " << getDistanceMax(solution, data, i_1, i_2) << std::endl;
+//        }
+//        duration = (std::clock() - start) / (double) CLOCKS_PER_SEC;
+//
+//    }
     //   Afficher les distances
-
-    std::cout << "__________________________ " << std::endl;
-    std::cout << "Distance max : " << getDistanceMax(solution, data, i_1, i_2) << std::endl;
+//
+//    std::cout << "__________________________ " << std::endl;
+//    std::cout << "Distance max : " << getDistanceMax(solution, data, i_1, i_2) << std::endl;
 //    for (auto item : data) {
 //        std::cout << "distance entre :" << item.getEntry().first.getId_() << " et " <<
 //        item.getEntry().second.getId_() << ":" << solution.getDistance(item.getEntry().first, item.getEntry().second) <<
 //        std::endl;
 //    }
-    WriteSolution(solution, file_in,data);
+    WriteSolution(solution, file_in, data);
     return 0;
 }
 
